18080e45d4a8ab51aa5f8a4cf9fc5255cf4cce69
vmxdev <vm@xenoeye.com>
2018-11-02 21:03:57 +0300

    Initial MTN (locked)

---
+++ mtn/tkvdb_mtn.c
@@ -0,0 +1,250 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+
+#include "tkvdb_mtn.h"
+
+
+struct tkvdb_mtn
+{
+	TKVDB_MTN_TYPE type;
+
+	union lock_t
+	{
+		pthread_mutex_t mutex;
+		pthread_spinlock_t spin;
+	} lock;
+
+	tkvdb_tr *tr;
+};
+
+tkvdb_mtn *
+tkvdb_mtn_create(tkvdb_tr *tr, TKVDB_MTN_TYPE type)
+{
+	tkvdb_mtn *mtn;
+
+	mtn = malloc(sizeof(tkvdb_mtn));
+	if (!mtn) {
+		goto fail_alloc;
+	}
+
+	switch (type) {
+		case TKVDB_MTN_MUTEX:
+		case TKVDB_MTN_MUTEX_TRY:
+			if (pthread_mutex_init(&mtn->lock.mutex, NULL) != 0) {
+				goto fail_lock;
+			}
+			break;
+		case TKVDB_MTN_SPINLOCK:
+		case TKVDB_MTN_SPINLOCK_TRY:
+			if (pthread_spin_init(&mtn->lock.spin, 0) != 0) {
+				goto fail_lock;
+			}
+			break;
+	}
+
+	mtn->type = type;
+	mtn->tr = tr;
+
+	return mtn;
+
+fail_lock:
+	free(mtn);
+fail_alloc:
+	return NULL;
+}
+
+
+void
+tkvdb_mtn_free(tkvdb_mtn *mtn)
+{
+	switch (mtn->type) {
+		case TKVDB_MTN_MUTEX:
+		case TKVDB_MTN_MUTEX_TRY:
+			pthread_mutex_destroy(&mtn->lock.mutex);
+			break;
+		case TKVDB_MTN_SPINLOCK:
+		case TKVDB_MTN_SPINLOCK_TRY:
+			pthread_spin_destroy(&mtn->lock.spin);
+			break;
+	}
+
+	free(mtn);
+}
+
+TKVDB_RES
+tkvdb_mtn_begin(tkvdb_mtn *mtn)
+{
+	TKVDB_RES rc;
+	int lr;
+
+	switch (mtn->type) {
+		case TKVDB_MTN_MUTEX:
+			pthread_mutex_lock(&mtn->lock.mutex);
+			rc = mtn->tr->begin(mtn->tr);
+			pthread_mutex_unlock(&mtn->lock.mutex);
+			break;
+
+		case TKVDB_MTN_MUTEX_TRY:
+			lr = pthread_mutex_trylock(&mtn->lock.mutex);
+			if (lr == EBUSY) {
+				return TKVDB_LOCKED;
+			}
+			rc = mtn->tr->begin(mtn->tr);
+			pthread_mutex_unlock(&mtn->lock.mutex);
+			break;
+
+		case TKVDB_MTN_SPINLOCK:
+			pthread_spin_lock(&mtn->lock.spin);
+			rc = mtn->tr->begin(mtn->tr);
+			pthread_spin_unlock(&mtn->lock.spin);
+			break;
+
+		case TKVDB_MTN_SPINLOCK_TRY:
+			lr = pthread_spin_trylock(&mtn->lock.spin);
+			if (lr == EBUSY) {
+				return TKVDB_LOCKED;
+			}
+			rc = mtn->tr->begin(mtn->tr);
+			pthread_spin_unlock(&mtn->lock.spin);
+			break;
+	}
+
+	return rc;
+}
+
+TKVDB_RES
+tkvdb_mtn_commit(tkvdb_mtn *mtn)
+{
+	TKVDB_RES rc;
+	int lr;
+
+	switch (mtn->type) {
+		case TKVDB_MTN_MUTEX:
+			pthread_mutex_lock(&mtn->lock.mutex);
+			rc = mtn->tr->commit(mtn->tr);
+			pthread_mutex_unlock(&mtn->lock.mutex);
+			break;
+
+		case TKVDB_MTN_MUTEX_TRY:
+			lr = pthread_mutex_trylock(&mtn->lock.mutex);
+			if (lr == EBUSY) {
+				return TKVDB_LOCKED;
+			}
+			rc = mtn->tr->commit(mtn->tr);
+			pthread_mutex_unlock(&mtn->lock.mutex);
+			break;
+
+		case TKVDB_MTN_SPINLOCK:
+			pthread_spin_lock(&mtn->lock.spin);
+			rc = mtn->tr->commit(mtn->tr);
+			pthread_spin_unlock(&mtn->lock.spin);
+			break;
+
+		case TKVDB_MTN_SPINLOCK_TRY:
+			lr = pthread_spin_trylock(&mtn->lock.spin);
+			if (lr == EBUSY) {
+				return TKVDB_LOCKED;
+			}
+			rc = mtn->tr->commit(mtn->tr);
+			pthread_spin_unlock(&mtn->lock.spin);
+			break;
+	}
+
+	return rc;
+}
+
+TKVDB_RES
+tkvdb_mtn_rollback(tkvdb_mtn *mtn)
+{
+	TKVDB_RES rc;
+	int lr;
+
+	switch (mtn->type) {
+		case TKVDB_MTN_MUTEX:
+			pthread_mutex_lock(&mtn->lock.mutex);
+			rc = mtn->tr->rollback(mtn->tr);
+			pthread_mutex_unlock(&mtn->lock.mutex);
+			break;
+
+		case TKVDB_MTN_MUTEX_TRY:
+			lr = pthread_mutex_trylock(&mtn->lock.mutex);
+			if (lr == EBUSY) {
+				return TKVDB_LOCKED;
+			}
+			rc = mtn->tr->rollback(mtn->tr);
+			pthread_mutex_unlock(&mtn->lock.mutex);
+			break;
+
+		case TKVDB_MTN_SPINLOCK:
+			pthread_spin_lock(&mtn->lock.spin);
+			rc = mtn->tr->rollback(mtn->tr);
+			pthread_spin_unlock(&mtn->lock.spin);
+			break;
+
+		case TKVDB_MTN_SPINLOCK_TRY:
+			lr = pthread_spin_trylock(&mtn->lock.spin);
+			if (lr == EBUSY) {
+				return TKVDB_LOCKED;
+			}
+			rc = mtn->tr->rollback(mtn->tr);
+			pthread_spin_unlock(&mtn->lock.spin);
+			break;
+	}
+
+	return rc;
+}
+
+TKVDB_RES
+tkvdb_mtn_put(tkvdb_mtn *mtn, const tkvdb_datum *key, const tkvdb_datum *val)
+{
+	TKVDB_RES rc;
+	int lr;
+
+	switch (mtn->type) {
+		case TKVDB_MTN_MUTEX:
+			pthread_mutex_lock(&mtn->lock.mutex);
+			rc = mtn->tr->put(mtn->tr, key, val);
+			pthread_mutex_unlock(&mtn->lock.mutex);
+			break;
+
+		case TKVDB_MTN_MUTEX_TRY:
+			lr = pthread_mutex_trylock(&mtn->lock.mutex);
+			if (lr == EBUSY) {
+				return TKVDB_LOCKED;
+			}
+			rc = mtn->tr->put(mtn->tr, key, val);
+			pthread_mutex_unlock(&mtn->lock.mutex);
+			break;
+
+		case TKVDB_MTN_SPINLOCK:
+			pthread_spin_lock(&mtn->lock.spin);
+			rc = mtn->tr->put(mtn->tr, key, val);
+			pthread_spin_unlock(&mtn->lock.spin);
+			break;
+
+		case TKVDB_MTN_SPINLOCK_TRY:
+			lr = pthread_spin_trylock(&mtn->lock.spin);
+			if (lr == EBUSY) {
+				return TKVDB_LOCKED;
+			}
+			rc = mtn->tr->put(mtn->tr, key, val);
+			pthread_spin_unlock(&mtn->lock.spin);
+			break;
+	}
+
+	return rc;
+}
+
+/*
+TKVDB_RES
+tkvdb_mtn_get(tkvdb_mtn *mtn, const tkvdb_datum *key, const tkvdb_datum *val)
+{
+}
+
+TKVDB_RES
+tkvdb_mtn_del(tkvdb_mtn *mtn, const tkvdb_datum *key, int del_pfx)
+{
+}
+*/
+
---
+++ mtn/tkvdb_mtn.h
@@ -0,0 +1,42 @@
+#ifndef tkvdb_mtn_h_included
+#define tkvdb_mtn_h_included
+
+#include "tkvdb.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum TKVDB_MTN_TYPE
+{
+	TKVDB_MTN_MUTEX,
+	TKVDB_MTN_MUTEX_TRY,
+	TKVDB_MTN_SPINLOCK,
+	TKVDB_MTN_SPINLOCK_TRY,
+	TKVDB_MTN_WAITFREE_SPMC
+} TKVDB_MTN_TYPE;
+
+typedef struct tkvdb_mtn tkvdb_mtn;
+
+tkvdb_mtn *tkvdb_mtn_create(tkvdb_tr *tr, TKVDB_MTN_TYPE type);
+void tkvdb_mtn_free(tkvdb_mtn *mtn);
+
+TKVDB_RES tkvdb_mtn_begin(tkvdb_mtn *mtn);
+TKVDB_RES tkvdb_mtn_commit(tkvdb_mtn *mtn);
+TKVDB_RES tkvdb_mtn_rollback(tkvdb_mtn *mtn);
+
+TKVDB_RES tkvdb_mtn_put(tkvdb_mtn *mtn,
+	const tkvdb_datum *key, const tkvdb_datum *val);
+
+TKVDB_RES tkvdb_mtn_get(tkvdb_mtn *mtn,
+	const tkvdb_datum *key, const tkvdb_datum *val);
+
+TKVDB_RES tkvdb_mtn_del(tkvdb_mtn *mtn, const tkvdb_datum *key, int del_pfx);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
