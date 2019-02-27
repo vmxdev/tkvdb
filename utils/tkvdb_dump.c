d916caa52faac33c5863b1d5147e27e590ae7daa
vmxdev <vm@xenoeye.com>
2019-02-27 13:08:52 +0300

    Add tkvdb-dump and tkvdb-restore utilities

---
+++ utils/tkvdb_dump.c
@@ -0,0 +1,249 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "tkvdb.h"
+
+/* min transaction buffer size */
+#define MIN_TR_SIZE 100000
+
+/* transaction size, 100M by default */
+#define DEF_TR_SIZE (100 * 1024 * 1024)
+static size_t trsize = DEF_TR_SIZE;
+
+static void
+print_sym(FILE *out, int sym)
+{
+	if (sym == '\n') {
+		fputs("\\n", out);
+	} else if (sym == '"') {
+		fputs("\\\"", out);
+	} else if (sym == '\\') {
+		fputs("\\\\", out);
+	} else if (sym == 0x1b) {
+		/* \e */
+		fputs("\\e", out);
+	} else {
+		fputc(sym, out);
+	}
+}
+
+static void
+print_kv_pair(FILE *out, tkvdb_cursor *c)
+{
+	size_t i;
+	uint8_t *data;
+
+	/* print key */
+	data = c->key(c);
+	fputc('"', out);
+	for (i=0; i<c->keysize(c); i++) {
+		print_sym(out, data[i]);
+	}
+	/* value */
+	data = c->val(c);
+	fputs("\":\"", out);
+	for (i=0; i<c->valsize(c); i++) {
+		print_sym(out, data[i]);
+	}
+	fputs("\"\n", out);
+}
+
+static void
+print_usage(char *progname)
+{
+	fprintf(stderr,
+		"Usage:\n %s [-o out_file] [-r] [-s size] db.tkvdb\n",
+		progname);
+	fprintf(stderr, " %s -h\n", progname);
+	fprintf(stderr, "    out_file - name of output file "\
+		"(default to stdout)\n");
+	fprintf(stderr, "    size - size of transaction buffer in bytes "\
+		"(default %zu, min %d)\n", (size_t)DEF_TR_SIZE, MIN_TR_SIZE);
+	fprintf(stderr, "    -r - dump in reverse order\n");
+	fprintf(stderr, "    -h - print this message\n");
+}
+
+int
+main(int argc, char *argv[])
+{
+	tkvdb *db;
+	tkvdb_tr *tr;
+	TKVDB_RES rc;
+	tkvdb_params *params;
+	tkvdb_cursor *c;
+
+	int opt;
+	int reverse = 0;
+	FILE *out = stdout;
+	char *outfile = NULL;
+	char *db_file;
+	int ret = EXIT_FAILURE;
+
+	uint8_t *last_key = NULL;
+	size_t last_key_size = 0;
+
+	while ((opt = getopt(argc, argv, "ho:rs:")) != -1) {
+		switch (opt) {
+			case 'o':
+				outfile = optarg;
+				break;
+			case 'r':
+				reverse = 1;
+				break;
+			case 's':
+				trsize = atoll(optarg);
+				break;
+
+			case 'h':
+			default:
+				print_usage(argv[0]);
+				return EXIT_SUCCESS;
+		}
+	}
+
+	if (trsize < MIN_TR_SIZE) {
+		print_usage(argv[0]);
+		return EXIT_SUCCESS;
+	}
+
+	if ((argc - optind) != 1) {
+		/* only one non-option argument */
+		print_usage(argv[0]);
+		return EXIT_SUCCESS;
+	}
+
+	db_file = argv[optind];
+
+	if (outfile) {
+		out = fopen(outfile, "w");
+		if (!out) {
+			fprintf(stderr, "Can't open output file '%s': %s",
+				outfile, strerror(errno));
+			goto fail_out;
+		}
+	}
+
+	/* init database parameters */
+	params = tkvdb_params_create();
+	if (!params) {
+		fprintf(stderr, "Can't create database parameters\n");
+		goto fail_params;
+	}
+
+	/* no dynamic reallocation of transaction buffer */
+	tkvdb_param_set(params, TKVDB_PARAM_TR_DYNALLOC, 0);
+	/* buffer size */
+	tkvdb_param_set(params, TKVDB_PARAM_TR_LIMIT, trsize);
+	/* don't try to create database, open read-only */
+#ifndef _WIN32
+	tkvdb_param_set(params, TKVDB_PARAM_DBFILE_OPEN_FLAGS, O_RDONLY);
+#else
+	tkvdb_param_set(params, TKVDB_PARAM_DBFILE_OPEN_FLAGS,
+		O_RDONLY | O_BINARY);
+#endif
+
+	/* open database */
+	db = tkvdb_open(db_file, params);
+	tkvdb_params_free(params);
+	if (!db) {
+		int err = errno;
+		fprintf(stderr, "Can't open db file '%s': %s\n",
+			db_file, err ? strerror(err): "corrupted database");
+		goto fail_dbopen;
+	}
+
+	/* create in-memory transaction with fixed buffer size */
+	tr = tkvdb_tr_create(db, NULL);
+	if (!tr) {
+		fprintf(stderr, "Can't create transaction\n");
+		goto fail_tr;
+	}
+
+	/* start transaction */
+	tr->begin(tr);
+
+	c = tkvdb_cursor_create(tr);
+	if (!c) {
+		fprintf(stderr, "Can't create cursor\n");
+		goto fail_cursor;
+	}
+
+	/* seek to first (or last) k-v pair */
+	rc = reverse ? c->last(c) : c->first(c);
+
+	for (;;) {
+		tkvdb_datum dtk;
+
+		while (rc == TKVDB_OK) {
+			print_kv_pair(out, c);
+
+			/* save last key */
+			dtk.size = c->keysize(c);
+			if (last_key_size < dtk.size) {
+				uint8_t *tmp;
+
+				tmp = realloc(last_key, dtk.size);
+				if (!tmp) {
+					fprintf(stderr, "realloc() failed\n");
+					goto fail_next;
+				}
+				last_key = tmp;
+				last_key_size = dtk.size;
+			}
+			dtk.data = last_key;
+			memcpy(last_key, c->key(c), dtk.size);
+
+			rc = reverse ? c->prev(c) : c->next(c);
+		}
+
+		if (rc == TKVDB_ENOMEM) {
+			/* transaction buffer overflow */
+
+			/* reset transaction */
+			tr->rollback(tr);
+			tr->begin(tr);
+
+			/* and start from last seen key */
+			rc = c->seek(c, &dtk, TKVDB_SEEK_EQ);
+			if (rc != TKVDB_OK) {
+				fprintf(stderr,
+					"seek() failed with code %d\n", rc);
+				goto fail_seek;
+			}
+
+			rc = reverse ? c->prev(c) : c->next(c);
+		} else if ((rc == TKVDB_NOT_FOUND) || (rc == TKVDB_EMPTY)) {
+			/* end of data or empty database */
+			break;
+		} else {
+			fprintf(stderr, "Error occured during dump,"\
+				" code %d\n", rc);
+			goto fail_next;
+		}
+	}
+
+	ret = EXIT_SUCCESS;
+
+fail_next:
+fail_seek:
+	free(last_key);
+	c->free(c);
+fail_cursor:
+	tr->free(tr);
+fail_tr:
+	tkvdb_close(db);
+fail_dbopen:
+fail_params:
+	if (outfile) {
+		fclose(out);
+	}
+fail_out:
+	return ret;
+}
+
---
+++ utils/tkvdb_restore.c
@@ -0,0 +1,357 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "tkvdb.h"
+
+/* min transaction buffer size */
+#define MIN_TR_SIZE 100000
+
+/* transaction size, 100M by default */
+#define DEF_TR_SIZE (100 * 1024 * 1024)
+
+#define SYM_APPEND(IN, C)            \
+do {                                 \
+	if (!sym_append(IN, C)) {    \
+		IN->error = 1;       \
+		return TOKEN_ENOMEM; \
+	}                            \
+} while (0)
+
+static size_t trsize = DEF_TR_SIZE;
+
+struct input
+{
+	FILE *f;
+	size_t line;
+
+	uint8_t *strtoken;
+	size_t toksize;
+	size_t tokallocated;
+
+	int error;
+};
+
+enum TOKEN
+{
+	TOKEN_EOF,
+	TOKEN_STRING,
+	TOKEN_COLON,
+	TOKEN_UNKNOWN,
+	TOKEN_ENOMEM
+};
+
+static int
+sym_append(struct input *in, int c)
+{
+	size_t new_size;
+	uint8_t *token;
+
+	if (in->toksize < in->tokallocated) {
+		/* we have enough room for next symbol */
+		in->strtoken[in->toksize] = c;
+		in->toksize++;
+		return 1;
+	}
+
+	new_size = in->tokallocated * 3 / 2;
+	if (new_size <= in->tokallocated) {
+		new_size = in->tokallocated + 1;
+	}
+
+	token = realloc(in->strtoken, new_size);
+	if (!token) {
+		/* XXX: free old data? */
+		return 0;
+	}
+
+	in->strtoken = token;
+	in->tokallocated = new_size;
+
+	/* append */
+	in->strtoken[in->toksize] = c;
+	in->toksize++;
+	return 1;
+}
+
+static int
+scan_until_eol(struct input *in)
+{
+	for (;;) {
+		int c;
+
+		c = fgetc(in->f);
+
+		if (c == '\n') {
+			return 1;
+		} else if (c == EOF) {
+			return 0;
+		}
+	}
+}
+
+static enum TOKEN
+scan_input(struct input *in)
+{
+	int c;
+
+again:
+	c = fgetc(in->f);
+	if (c == EOF) {
+		return TOKEN_EOF;
+	} else if (c == '#') {
+		/* comment */
+		if (!scan_until_eol(in)) {
+			return TOKEN_EOF;
+		}
+		in->line++;
+		goto again;
+	} else if ((c == ' ') || (c == '\t')) {
+		/* whitespace */
+		goto again;
+	} else if (c == '\n') {
+		/* new line */
+		in->line++;
+		goto again;
+	} else if (c == '"') {
+		/* string (key or value) */
+		for (;;) {
+			c = fgetc(in->f);
+			if (c == EOF) {
+				/* unexpected EOF */
+				in->error = 1;
+				return TOKEN_EOF;
+			} else if (c == '"') {
+				/* end of string */
+				return TOKEN_STRING;
+			} else if (c == '\\') {
+				/* escaped symbol */
+				c = fgetc(in->f);
+				if (c == EOF) {
+					/* unexpected EOF */
+					in->error = 1;
+					return TOKEN_EOF;
+				} else if ((c == '\\') || (c == '"')) {
+					/* append escaped symbol */
+					SYM_APPEND(in, c);
+				} else if (c == 'n') {
+					SYM_APPEND(in, '\n');
+				} else if (c == 'e') {
+					SYM_APPEND(in, 0x1b);
+				}
+			} else {
+				/* append symbol */
+				SYM_APPEND(in, c);
+			}
+		}
+	} else if (c == ':') {
+		return TOKEN_COLON;
+	} else {
+		/* unknown token */
+		in->error = 1;
+		if (!scan_until_eol(in)) {
+			return TOKEN_EOF;
+		}
+		in->line++;
+		return TOKEN_UNKNOWN;
+	}
+}
+
+static int
+add_pair(struct input *in, tkvdb_tr *tr)
+{
+	enum TOKEN token;
+	uint8_t *key, *val;
+	size_t keysize, valsize;
+	tkvdb_datum dtk, dtv;
+	TKVDB_RES rc;
+
+	token = scan_input(in);
+	if (token == TOKEN_EOF) {
+		if (in->error) {
+			fprintf(stderr, "Unexpected EOF at line %lu\n",
+				in->line);
+		}
+		return 0;
+	} else if (token != TOKEN_STRING) {
+		fprintf(stderr, "Expected quoted key at line %lu\n", in->line);
+		return 0;
+	}
+
+	key = in->strtoken;
+	keysize = in->toksize;
+
+	token = scan_input(in);
+	if (token != TOKEN_COLON) {
+		fprintf(stderr, "Expected ':' after key at line %lu\n",
+			in->line);
+		return 0;
+	}
+
+	token = scan_input(in);
+	if (token != TOKEN_STRING) {
+		fprintf(stderr, "Expected value after ':' at line %lu\n",
+			in->line);
+		return 0;
+	}
+
+	val = in->strtoken + keysize;
+	valsize = in->toksize - keysize;
+
+	dtk.data = key;
+	dtk.size = keysize;
+	dtv.data = val;
+	dtv.size = valsize;
+
+	rc = tr->put(tr, &dtk, &dtv);
+	if (rc == TKVDB_ENOMEM) {
+		rc = tr->commit(tr);
+		if (rc != TKVDB_OK) {
+			fprintf(stderr, "commit() failed with code %d\n", rc);
+			return 0;
+		}
+		tr->begin(tr);
+
+		/* try again */
+		rc = tr->put(tr, &dtk, &dtv);
+	}
+
+	if (rc != TKVDB_OK) {
+		fprintf(stderr, "put() failed with code %d\n", rc);
+		return 0;
+	}
+
+	in->error = 0;
+	/* reset input token */
+	in->toksize = 0;
+
+	return 1;
+}
+
+static void
+print_usage(char *progname)
+{
+	fprintf(stderr,
+		"Usage:\n %s [-i in_file] [-s size] db.tkvdb\n",
+		progname);
+	fprintf(stderr, " %s -h\n", progname);
+	fprintf(stderr, "    in_file - name of input dump file "\
+		"(default stdin)\n");
+	fprintf(stderr, "    size - size of transaction buffer in bytes "\
+		"(default %zu, min %d)\n", (size_t)DEF_TR_SIZE, MIN_TR_SIZE);
+	fprintf(stderr, "    -h - print this message\n");
+}
+
+int
+main(int argc, char *argv[])
+{
+	tkvdb *db;
+	tkvdb_tr *tr;
+	tkvdb_params *params;
+
+	int opt;
+	char *infile = NULL;
+	char *db_file;
+	int ret = EXIT_FAILURE;
+
+	struct input in;
+
+	in.f = stdin;
+	in.line = 1;
+	in.strtoken = NULL;
+	in.toksize = in.tokallocated = 0;
+	in.error = 0;
+
+	while ((opt = getopt(argc, argv, "hi:s:")) != -1) {
+		switch (opt) {
+			case 'i':
+				infile = optarg;
+				break;
+			case 's':
+				trsize = atoll(optarg);
+				break;
+
+			case 'h':
+			default:
+				print_usage(argv[0]);
+				return EXIT_SUCCESS;
+		}
+	}
+
+	if (trsize < MIN_TR_SIZE) {
+		print_usage(argv[0]);
+		return EXIT_SUCCESS;
+	}
+
+	if ((argc - optind) != 1) {
+		/* only one non-option argument */
+		print_usage(argv[0]);
+		return EXIT_SUCCESS;
+	}
+
+	db_file = argv[optind];
+
+	if (infile) {
+		in.f = fopen(infile, "r");
+		if (!in.f) {
+			fprintf(stderr, "Can't open input file '%s': %s",
+				infile, strerror(errno));
+			goto fail_in;
+		}
+	}
+
+	/* init database parameters */
+	params = tkvdb_params_create();
+	if (!params) {
+		fprintf(stderr, "Can't create database parameters\n");
+		goto fail_params;
+	}
+
+	/* no dynamic reallocation of transaction buffer */
+	tkvdb_param_set(params, TKVDB_PARAM_TR_DYNALLOC, 0);
+	/* buffer size */
+	tkvdb_param_set(params, TKVDB_PARAM_TR_LIMIT, trsize);
+
+	/* open database */
+	db = tkvdb_open(db_file, params);
+	tkvdb_params_free(params);
+	if (!db) {
+		int err = errno;
+		fprintf(stderr, "Can't open db file '%s': %s\n",
+			db_file, err ? strerror(err): "corrupted database");
+		goto fail_dbopen;
+	}
+
+	/* create in-memory transaction with fixed buffer size */
+	tr = tkvdb_tr_create(db, NULL);
+	if (!tr) {
+		fprintf(stderr, "Can't create transaction\n");
+		goto fail_tr;
+	}
+
+	tr->begin(tr);
+
+	while (add_pair(&in, tr)) {};
+
+	tr->commit(tr);
+
+	ret = EXIT_SUCCESS;
+
+	free(in.strtoken);
+	tr->free(tr);
+fail_tr:
+	tkvdb_close(db);
+fail_dbopen:
+fail_params:
+	if (infile) {
+		fclose(in.f);
+	}
+fail_in:
+	return ret;
+}
+
